8102dd8689218d8ad0dc5503e081f96a9002369b
Dannit23 <danalexa23@gmail.com>
2022-10-13 03:28:13 -0500

    se realiza el componente Navbar y Carousel, aunque falta terminar el carousel

--- prueba-it-globers/src/Components/Carousel.js
+++ prueba-it-globers/src/Components/Carousel.js
@@ -1,10 +1,36 @@
-
-
+import React from 'react'
 
 const Carousel = () => {
     return (
-        <div className="Carousel">
-           
+        <div id='carouselExampleIndicators' class="carousel slide" data-bs-ride="true">
+            <div class="carousel-indicators">
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://i.imgur.com/3qJCiKk.jpg" class="d-block w-100" alt="Paisaje"></img>
+                </div>
+                <div class="carousel-item">
+                  <img src="https://i.imgur.com/NDxndA3.jpg" class="d-block w-100" alt="Mujeres en el mar"></img>
+                </div>
+                <div class="carousel-item">
+                  <img src="https://i.imgur.com/pdsrdSp.jpg" class="d-block w-100" alt="Mujer descansando"></img>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+            <div>
+               <h1>ESTÁR CÓMODO NUNCA FUE TAN FÁCIL</h1>
+               <button>SHOP</button>
+            </div>
         </div>
     );
 };
--- prueba-it-globers/src/Components/Navbar.js
+++ prueba-it-globers/src/Components/Navbar.js
@@ -15,14 +15,35 @@ return (
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#timePuff">
-                        <img src='https://i.imgur.com/hq8L0KI.png' alt='Logo'></img>
+                        <img src='https://i.imgur.com/hq8L0KI.png' alt='LogoPuff'></img>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#timeRain">
+                        <img src='https://i.imgur.com/vLM6mbD.png' alt='LogoSombrilla'></img>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#timeCart">
+                        <img src='https://i.imgur.com/BcrNi0M.png' alt='LogoBolso'></img>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#timeNap">
+                        <img src='https://i.imgur.com/D5cBtA4.png' alt='LogoAlmohada'></img>
                     </a>
                 </li>
             </ul>
         </div>
+        <div className='nav-menu-dropdown'>
+            <select id="my-account">
+              <option value="my-account">MI CUENTA</option>
+            </select>
+            <a class="my-shopping" href="#">MI COMPRA</a>
+        </div>
    </nav>
-)
+);
 
-}
+};
 
 export default Navbar
